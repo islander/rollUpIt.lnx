98aa046bc0d1f74d5177dab20db786c3c2e82f84
Ilya Likhobabin <gonzo.soc@gmail.com>
2017-08-12 13:25:47 +0000

    Merge branches

--- test_net.sh
+++ test_net.sh
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 #! /bin/bash
 
 source $(pwd)/libs/net.sh
 
 flushNetwork
-=======
-#!/bin/bash
-
-source $(pwd)/libs/net.sh
-#1
-flushNetworks 
->>>>>>> 4027d7ba9b659268682be2af3b855ae06236f0ee
